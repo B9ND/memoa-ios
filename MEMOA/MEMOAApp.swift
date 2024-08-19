f5c2a7b86db5e6078bb332a3b3085f10fb9ed54a
eunchan2815 <164847037+eunchan2815@users.noreply.github.com>
2024-08-19 20:33:50 +0900

    feat: add gitignore

---
+++ MEMOA/MEMOAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEMOAApp.swift
+//  MEMOA
+//
+//  Created by dgsw30 on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MEMOAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
